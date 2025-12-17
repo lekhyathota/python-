eb144c69bea4fa7fc1840236ededa17d16d368f7
lekhyathota <lekhyathota64@gmail.com>
2025-12-17 19:50:45 +0530

    datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,33 @@
+# Python Data Types
+
+# Integer
+a = 10
+print("Integer:", a)
+
+# Float
+b = 10.5
+print("Float:", b)
+
+# String
+name = "Python"
+print("String:", name)
+
+# Boolean
+is_python_easy = True
+print("Boolean:", is_python_easy)
+
+# List
+numbers = [1, 2, 3, 4]
+print("List:", numbers)
+
+# Tuple
+colors = ("red", "green", "blue")
+print("Tuple:", colors)
+
+# Set
+unique_numbers = {1, 2, 3, 3}
+print("Set:", unique_numbers)
+
+# Dictionary
+student = {"name": "Lekhya", "age": 21}
+print("Dictionary:", student)
