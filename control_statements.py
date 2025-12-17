de79c0c934602fa43e5e043ef40ab29f5140f0f2
lekhyathota <lekhyathota64@gmail.com>
2025-12-17 19:55:14 +0530

    control_statements.py

---
+++ control_statements.py
@@ -0,0 +1,17 @@
+# If-Else
+age = 18
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible")
+
+# For Loop
+for i in range(1, 6):
+    print(i)
+
+# While Loop
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
